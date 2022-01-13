bfd92842f71f9c464db355a47c94e1d11731c6a5
deer-li <1597493814@qq.com>
2022-01-13 14:23:39 +0800

    java core tech first commit

---
+++ src/main/java/com/think/java02/ATypeName.java
@@ -0,0 +1,12 @@
+package com.think.java02;
+public class ATypeName {
+    int a=1,b=1;
+    public int sum(){
+        int i=a+b;
+        return i;
+    }
+    public static void main(String[] args) {
+        ATypeName aTypeName=new ATypeName();
+        System.out.println(aTypeName.sum());
+    }
+}
---
+++ src/main/java/com/think/java02/annotationTohtml.java
@@ -0,0 +1,22 @@
+package com.think.java02;
+import java.util.Date;
+//javadoc 命令可以生成API文档
+/**
+ * The first Think in java example program
+ * Display a string and today's date.
+ * @author Bruce Eckel
+ * @version 4.0
+ */
+public class  annotationTohtml
+{
+    /**
+     *Entrv point to class & application.
+     *@param args array of String arguments.
+     *@throws
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, ist's: ");
+        System.out.println(new Date());
+    }
+}
