afe70690d91dc228df45b3c51e338c35ecac3bf6
deer-li <1597493814@qq.com>
2022-01-21 16:20:03 +0800

    2022/1/21

--- src/main/java/com/think/java03/Retirement.java
+++ src/main/java/com/think/java03/Retirement.java
@@ -1,11 +1,33 @@
 package com.think.java03;
 
+import java.util.Scanner;
+
 /**
  * @Classname Retirement
- * @Description TODO
+ * @Description while循环
  * @Version 1.0.0
  * @Date 2022/1/13 21:04
  * @Created by deer-li
  */
 public class Retirement {
+    public static void main(String[] args) {
+        //read input
+        Scanner in = new Scanner(System.in);
+        System.out.print("How much money do you need to retire?");
+        double goal = in.nextDouble();
+        System.out.print("How much money will you contribute every year?");
+        double payment = in.nextDouble();
+        System.out.print("Interest rate in %:");
+        double interestRate = in.nextDouble();
+
+        double balance=0;
+        int years=0;
+        while (balance<goal){
+            balance=balance+payment;
+            double interest=balance*interestRate/100;
+            balance=balance+interest;
+            years++;
+        }
+        System.out.println("You can retire in"+years+"years.");
+    }
 }
---
+++ src/main/java/com/think/java04/CalendarTest.java
@@ -0,0 +1,12 @@
+package com.think.java04;
+
+/**
+ * @Classname CalendarTest
+ * @Description TODO
+ * @Version 1.0.0
+ * @Date 2022/1/21 16:18
+ * @Created by deer-li
+ */
+public class CalendarTest {
+
+}
