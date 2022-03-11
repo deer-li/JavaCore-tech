dd31b7db33e9fe265b41587b8fac63326c933ae9
deer-li <1597493814@qq.com>
2022-03-11 10:09:57 +0800

    '1022-03-11'

---
+++ src/main/java/designpattern/SingleObject.java
@@ -0,0 +1,11 @@
+package designpattern;
+
+/**
+ * @Classname SingleObject
+ * @Description TODO
+ * @Version 1.0.0
+ * @Date 2022/3/11 10:01
+ * @Created by deer-li
+ */
+public class SingleObject {
+}
---
+++ src/main/java/designpattern/SingletonPatternDemo.java
@@ -0,0 +1,11 @@
+package designpattern;
+
+/**
+ * @Classname SingletonPatternDemo
+ * @Description TODO
+ * @Version 1.0.0
+ * @Date 2022/3/11 10:00
+ * @Created by deer-li
+ */
+public class SingletonPatternDemo {
+}
