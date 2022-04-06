3cfe388c55dcf3dd7a65b9c27786f4dd7cf21359
deer-li <1597493814@qq.com>
2022-04-06 15:53:17 +0800

    LocalDate实现日历

--- src/main/java/com/think/java04/CalendarTest.java
+++ src/main/java/com/think/java04/CalendarTest.java
@@ -1,4 +1,7 @@
 package com.think.java04;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.*;
 /**
  * @Classname CalendarTest
  * @Description TODO
@@ -8,7 +11,27 @@ package com.think.java04;
  */
 public class CalendarTest {
     public static void main(String[] args) {
+        LocalDate date = LocalDate.now();
+        int month=date.getMonthValue();
+        int today=date.getDayOfMonth();
+        date=date.minusDays(today-1);
+        DayOfWeek weekday = date.getDayOfWeek();
+        int value=weekday.getValue();
+        System.out.println("Mon Tue Wed Thu Fri Sat Sun");
+        for (int i=1;i<value;i++){
+            System.out.print("    ");
+        }
+        while (date.getMonthValue()==month){
+            System.out.printf("%3d",date.getDayOfMonth());
+            if (date.getDayOfMonth()==today){
+                System.out.print("*");
+            }else {
+                System.out.print(" ");
+            }
+            date=date.plusDays(1);
+            if (date.getDayOfWeek().getValue()==1) System.out.println();
+        }
+        if (date.getDayOfWeek().getValue()!=1) System.out.println();
 
     }
-
 }
