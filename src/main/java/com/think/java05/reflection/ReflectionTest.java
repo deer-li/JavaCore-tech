598697e9368db69d22fbde30d90933b3424495ac
deer-li <1597493814@qq.com>
2022-04-11 09:40:17 +0800

    \

--- src/main/java/com/think/java05/reflection/ReflectionTest.java
+++ src/main/java/com/think/java05/reflection/ReflectionTest.java
@@ -14,6 +14,7 @@ import java.util.Scanner;
  * @Date 2022/4/10 19:37
  * @Created by deer-li
  */
+
 public class ReflectionTest {
     public static void main(String[] args) throws ReflectiveOperationException {
         String name;
