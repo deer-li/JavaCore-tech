fceb77880f4ac73b08f3815685210a5df4b727b1
deer-li <1597493814@qq.com>
2022-04-12 15:44:10 +0800

    timer

---
+++ src/main/java/com/think/java06/clone/CloneTest.java
@@ -0,0 +1,14 @@
+package com.think.java06.clone;
+
+/**
+ * @Classname CloneTest
+ * @Description TODO
+ * @Version 1.0.0
+ * @Date 2022/4/12 15:35
+ * @Created by deer-li
+ */
+public class CloneTest {
+
+
+}
+
---
+++ src/main/java/com/think/java06/clone/Employee.java
@@ -0,0 +1,12 @@
+package com.think.java06.clone;
+
+/**
+ * @Classname Employee
+ * @Description TODO
+ * @Version 1.0.0
+ * @Date 2022/4/12 15:35
+ * @Created by deer-li
+ */
+public class Employee {
+
+}
