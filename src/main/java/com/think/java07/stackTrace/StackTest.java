3c85b64a920962d7ae11fab82b6e58579d754790
deer-li <1597493814@qq.com>
2022-04-18 15:15:39 +0800

    stackwalker

---
+++ src/main/java/com/think/java07/stackTrace/StackTest.java
@@ -0,0 +1,37 @@
+package com.think.java07.stackTrace;
+import java.util.*;
+/**
+ * @Classname StackTest
+ * @Description TODO
+ * @Version 1.0.0
+ * @Date 2022/4/18 14:51
+ * @Created by deer-li
+ */
+public class StackTest
+{
+    /**
+     * Computes the factorial of a number
+     * @param n a non-negative integer
+     * @return n! = 1 * 2 * . . . * n
+     */
+    public static int factorial(int n)
+    {
+        System.out.println("factorial(" + n + "):");
+        StackWalker walker = StackWalker.getInstance();
+        walker.forEach(System.out::println);
+        int r;
+        if (n <= 1) r = 1;
+        else r = n * factorial(n - 1);
+        System.out.println("return " + r);
+        return r;
+    }
+    public static void main(String[] args)
+    {
+        try (Scanner in = new Scanner(System.in))
+        {
+            System.out.print("Enter n: ");
+            int n = in.nextInt();
+            factorial(n);
+        }
+    }
+}
