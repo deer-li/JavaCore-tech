39d60648358080c0aa90a4c6c495fabb439772ed
deer-li <1597493814@qq.com>
2022-04-19 16:17:54 +0800

    泛型之类型擦除

--- src/main/java/com/think/java08/pair2/PairTest2.java
+++ src/main/java/com/think/java08/pair2/PairTest2.java
@@ -1,7 +1,6 @@
 package com.think.java08.pair2;
 
 import com.think.java08.pair1.Pair;
-
 import java.time.LocalDate;
 
 /**
@@ -44,3 +43,6 @@ class ArrayAlg{
 
     }
 }
+//正确理解泛型擦除的概念
+//：编译期间所有的泛型信息都会被擦掉，java实现泛型是在编译器层次上实现的，
+//在生成的字节码中是不包含泛型中的类型信息的，使用泛型的时候加上类型参数，在编译器编译的时候会去掉，
