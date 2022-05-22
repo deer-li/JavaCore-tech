8d72f9e5f459dcf784e8b6e91bb742dd4d33e03c
deer-li <1597493814@qq.com>
2022-05-22 14:28:02 +0800

    01

--- src/main/java/com/think2/java02/textFile/TextFileTest.java
+++ src/main/java/com/think2/java02/textFile/TextFileTest.java
@@ -14,6 +14,7 @@ import java.util.Scanner;
  * @Date 2022/5/22 10:48
  * @Created by deer-li
  */
+
 public class TextFileTest {
     public static void main(String[] args) throws IOException {
         Employee[] staff = new Employee[3];
